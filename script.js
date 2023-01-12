21fd48cb066806674772123c18093d5ff9d8f578
markymelnik <markemelnik@gmail.com>
2023-01-12 17:05:46 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Practice with Algorithms
+
+// Display
+
+const container = document.querySelector('.container');
+const output = document.createElement('div');
+output.classList.add('output');
+output.textContent = "Output";
+container.append(output);
